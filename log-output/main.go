49ade4d99396475e51ef930f1df8432ed61793f4
arkuar <arttukuronen@gmail.com>
2021-11-04 17:55:11 +0200

    1.07

--- log-output/main.go
+++ log-output/main.go
@@ -2,15 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"time"
 
 	"github.com/google/uuid"
 )
 
+var (
+	output      uuid.UUID
+	currentTime string
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "%s: %s", currentTime, output.String())
+}
+
 func main() {
-	output, _ := uuid.NewRandom()
+	output, _ = uuid.NewRandom()
 	ticker := time.NewTicker(time.Second * 5)
-	for ; true; <-ticker.C {
-		fmt.Printf("%s: %s\n", time.Now().Format(time.RFC1123), output.String())
-	}
+
+	http.HandleFunc("/", handler)
+
+	go func() {
+		for ; true; <-ticker.C {
+			currentTime = time.Now().Format(time.RFC1123)
+			fmt.Printf("%s: %s\n", currentTime, output.String())
+		}
+	}()
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
