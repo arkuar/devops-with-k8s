b87d2790d5c52923ce0d41bc42a433967921341e
arkuar <arttukuronen@gmail.com>
2021-11-05 19:31:40 +0200

    1.11

--- log-output/reader/main.go
+++ log-output/reader/main.go
@@ -7,12 +7,20 @@ import (
 	"os"
 )
 
-func handler(w http.ResponseWriter, r *http.Request) {
-	data, err := os.ReadFile("/usr/src/app/files/output")
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprintf(w, "%s", string(data))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	data, err := os.ReadFile("/usr/src/app/files/output")
+	checkErr(err)
+
+	pongs, err := os.ReadFile("/usr/src/app/files/pongs")
+	checkErr(err)
+
+	fmt.Fprintf(w, "%s\n%s", string(data), string(pongs))
 }
 
 func main() {
--- ping-pong/pong.go
+++ ping-pong/pong.go
@@ -4,16 +4,38 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 var counter int = 0
+var dir = "/usr/src/app/files"
+
+func writeToFile(count int) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err := os.Mkdir(dir, 0755)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	f, err := os.Create(dir + "/pongs")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	fmt.Fprintf(f, "Ping / Pongs: %d", count)
+}
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "pong %d", counter)
+	writeToFile(counter)
 	counter++
 }
 
 func main() {
+	// Initial write
+	writeToFile(counter)
+
 	http.HandleFunc("/", handler)
 	log.Fatal(http.ListenAndServe(":3000", nil))
 }
