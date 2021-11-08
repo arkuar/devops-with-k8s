25e4bbe7bae428369efb1f6a3f7d58c1ca9c6d07
arkuar <arttukuronen@gmail.com>
2021-11-08 15:42:09 +0200

    1.12

--- project/app.go
+++ project/app.go
@@ -1,12 +1,40 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
 )
+
+func check(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func fetchImage() {
+	if _, err := os.Stat("/usr/src/app/cache/image.jpg"); errors.Is(err, os.ErrNotExist) {
+		resp, err := http.Get("https://picsum.photos/1200")
+		check(err)
+
+		defer resp.Body.Close()
+
+		file, err := os.Create("/usr/src/app/cache/image.jpg")
+		check(err)
+
+		defer file.Close()
+
+		_, err = io.Copy(file, resp.Body)
+		check(err)
+
+		log.Println("Image fetched and cached")
+	}
+}
 
 func main() {
 	port := os.Getenv("PORT")
@@ -14,8 +42,10 @@ func main() {
 		port = "8080"
 	}
 	router := gin.Default()
-	router.LoadHTMLGlob("public/index.html")
+	router.StaticFile("image.jpg", "/usr/src/app/cache/image.jpg")
+	router.LoadHTMLGlob("public/*.html")
 	router.GET("/", func(c *gin.Context) {
+		fetchImage()
 		c.HTML(http.StatusOK, "index.html", nil)
 	})
 
